0c54b71702384ad37a672eaac638d14096eca69e
Ken Chau <kchau@microsoft.com>
2020-12-14 11:57:48 -0800

    init commit

---
+++ packages/app1/babel.config.js
@@ -0,0 +1,18 @@
+const isServer = process.env.SERVER !== undefined
+
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: isServer
+          ? {
+              node: true
+            }
+          : '> 0.25%, not dead'
+      }
+    ],
+    '@babel/preset-react'
+  ],
+  plugins: ['@loadable/babel-plugin']
+}
